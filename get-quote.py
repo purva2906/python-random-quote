cf9f78f5134aeb45d56a73f9df63ab9252670f70
purva2906 <69680501+purva2906@users.noreply.github.com>
2020-08-31 20:41:35 +0530

    git add get-quote.py
    git commit -m "Random quote bot is working"

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,13 @@
+import random
 def main():
+  last = 13
+  rnd = random.randint(0, last)
   print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
+print(quotes[rnd]
   print(quotes)
 
 if __name__== "__main__":
